9e7a9daa5326e88d7fcfcf160c904e9676db6105
Max <max.luetkemeyer@outlook.de>
2024-10-02 01:06:06 +0200

    Kickoff TTL

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,6 +1,15 @@
 package store
 
+import "time"
+
 type Store interface {
 	SetValue(key string, value string)
 	GetValue(key string) (value string, error error)
+}
+
+type TTLStore interface {
+	SetValue(key string, value string, ttl time.Duration)
+	GetValue(key string) (value string, error error)
+	SetTTL(key string, ttl time.Duration) error
+	GetTTL(key string) (time.Duration, error)
 }
