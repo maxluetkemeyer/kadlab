4d5c0a96dae9a518ff107e75d8906bca193e5523
Johnny Lam <johnnylam0914@gmail.com>
2024-10-10 16:45:36 +0200

    rest endpoints (#67)
    * increase bootstrap timeout, fix findNodes adding bad nodes
    
    * node findValue
    
    * replicate to closest node without data
    
    * fix deadlock
    
    * rest api
    
    * contact json marshal

--- api/standard_http_api.go
+++ api/standard_http_api.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	"d7024e_group04/env"
 	"d7024e_group04/internal/node"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"strconv"
@@ -24,8 +28,8 @@ func (h *Handler) ListenAndServe(ctx context.Context) error {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /object/", h.getObject())
-	mux.HandleFunc("POST /object", h.putObject())
+	mux.HandleFunc("GET /objects/{hash}", h.getObject())
+	mux.HandleFunc("POST /objects", h.putObject())
 
 	go func() {
 		log.Printf("serving http requests on port %v", env.ApiPort)
@@ -44,12 +48,64 @@ func (h *Handler) ListenAndServe(ctx context.Context) error {
 
 func (h *Handler) getObject() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		panic("TODO")
+		hash := r.PathValue("hash")
+		// check if hex value
+		_, err := hex.DecodeString(hash)
+		if err != nil || len(hash) != 40 { // length of sha-1
+			http.Error(w, "invalid hash", http.StatusUnprocessableEntity)
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(r.Context(), env.RPCTimeout)
+		value, candidates, err := h.node.GetObject(ctx, hash)
+		cancel()
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if value != nil {
+			json.NewEncoder(w).Encode(value)
+			return
+		}
+
+		if candidates != nil {
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(candidates)
+		}
+
 	}
 }
 
 func (h *Handler) putObject() func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		panic("TODO")
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Failed to read request body", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+
+		data := string(body)
+
+		ctx, cancel := context.WithTimeout(r.Context(), env.RPCTimeout)
+		hash, err := h.node.PutObject(ctx, data)
+		cancel()
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Location", fmt.Sprintf("/objects/%v", hash))
+		w.WriteHeader(http.StatusCreated)
+
+		response := map[string]string{
+			"data": data,
+		}
+
+		json.NewEncoder(w).Encode(response)
 	}
 }
--- internal/kademlia/model/model.go
+++ internal/kademlia/model/model.go
@@ -3,6 +3,6 @@ package model
 import "d7024e_group04/internal/kademlia/contact"
 
 type FindValueSuccessfulResponse struct {
-	DataValue     string
-	NodeWithValue *contact.Contact
+	DataValue     string           `json:"Value"`
+	NodeWithValue *contact.Contact `json:"Node"`
 }
