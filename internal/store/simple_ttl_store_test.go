92e7454e8945859ad14119c95d3e83c539bc7c41
Max <max.luetkemeyer@outlook.de>
2024-10-11 20:09:35 +0200

    More tests

--- internal/store/simple_ttl_store_test.go
+++ internal/store/simple_ttl_store_test.go
@@ -36,7 +36,7 @@ func TestSimpleTTLStore(t *testing.T) {
 
 		got, err := tStore.GetValue("key0")
 
-		if got == val {
+		if got != "" {
 			log.Fatalf("should get empty string, but got %v", got)
 		}
 
@@ -44,5 +44,36 @@ func TestSimpleTTLStore(t *testing.T) {
 			log.Fatalf("should error, but got nil")
 		}
 
+	})
+
+	t.Run("set value with enough ttl and later it should be expired", func(t *testing.T) {
+		memStore := NewMemoryStore()
+		tStore := NewSimpleTTLStore(memStore)
+
+		val := "value0"
+
+		tStore.SetValue("key0", val, time.Hour)
+
+		got, err := tStore.GetValue("key0")
+
+		if err != nil {
+			log.Fatalf("error getting value: %v", err)
+		}
+
+		if got != val {
+			log.Fatalf("TTL should be enough: got %q, want %q", got, val)
+		}
+
+		tStore.SetTTL("key0", time.Second*0)
+
+		gotAfterExpiration, errAfterExpiration := tStore.GetValue("key0")
+
+		if gotAfterExpiration != "" {
+			log.Fatalf("should get empty string, but got %v", gotAfterExpiration)
+		}
+
+		if errAfterExpiration == nil {
+			log.Fatalf("should error, but got nil")
+		}
 	})
 }
